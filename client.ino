387f8cd259c2f1494df5c28e123165af17374e2c
wfsbear <47589206+ilikecinepol@users.noreply.github.com>
2023-09-05 22:41:29 +0300

    Update client.ino

--- client.ino
+++ client.ino
@@ -1,5 +1,7 @@
 void setup() {
   Serial.begin(115200);
+  pinMode(13, OUTPUT); // Устанавливаем 13-й пин как выход для светодиода
+  digitalWrite(13, 1); // Изначально выключаем светодиод
 }
 
 void loop() {
@@ -10,9 +12,18 @@ void loop() {
     if (receivedChar == '\n') {  // Если символ - перевод строки, обработать сообщение
       Serial.print("Received: ");
       Serial.println(receivedString);
-      receivedString = "";  // Очистить строку для следующего сообщения
+      
+      // Проверяем, равно ли принятое сообщение "LED ON"
+      if (receivedString == "LED ON") {
+        digitalWrite(13, HIGH); // Включаем светодиод
+      }
+      else if (receivedString == "LED OFF") {
+        digitalWrite(13, LOW); // Включаем светодиод
+      }
+      
+      receivedString = "";  // Очищаем строку для следующего сообщения
     } else {
-      receivedString += receivedChar;  // Добавить символ к строке
+      receivedString += receivedChar;  // Добавляем символ к строке
     }
   }
 }
